38e8f490764acf90c82bb2814f428f33a6f4dff7
Javier CS <jcampallosequera@gmail.com>
2022-01-18 10:05:24 +0100

    Mejora de la estructura del controlador principal

--- bestipesCook/src/main/java/ctrl/Ctrl_Login.java
+++ bestipesCook/src/main/java/ctrl/Ctrl_Login.java
@@ -5,7 +5,7 @@ import view.Login;
 public class Ctrl_Login {
 
 	public static void conectar() {
-		if(Login.txtUsuario.getText().equals("admin") && String.valueOf(Login.txtPassword.getPassword()).equals("admin")) {
+		if(Login.txtUsuario.getText().equals("") && String.valueOf(Login.txtPassword.getPassword()).equals("")) {
 			Login.window.dispose();
 		}
 	}
