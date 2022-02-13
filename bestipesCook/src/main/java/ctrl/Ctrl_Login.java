10ecc43b4d66ce657020a20371a43fbb98ca9e06
Luis Montero <lmongarman@gmail.com>
2022-02-13 02:12:50 +0100

    Implementacion de funcionalidad a Login. close #29
    Se ha dado funcionalidad al Login hasheando la contraseña y mandando por POST.
    
    Es un poco arcaico el método pero funciona.

--- bestipesCook/src/main/java/ctrl/Ctrl_Login.java
+++ bestipesCook/src/main/java/ctrl/Ctrl_Login.java
@@ -1,17 +1,37 @@
 package ctrl;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+import javax.swing.JOptionPane;
+
+import com.google.common.hash.Hashing;
+
 import view.Login;
+import view.RecetaDetalle;
 
 public class Ctrl_Login {
 
 	public static void conectar() {
-		if(Login.txtUsuario.getText().equals("") && String.valueOf(Login.txtPassword.getPassword()).equals("")) {
-			Login.window.dispose();
+		
+		String sha256hexPass = Hashing.sha256()
+				  .hashString(String.valueOf(Login.txtPassword.getPassword()), StandardCharsets.UTF_8)
+				  .toString().toUpperCase();
+		
+		try {
+			if (logic.LoginLogic.verificarAdmin(Login.txtUsuario.getText(), sha256hexPass) /*|| ( Login.txtUsuario.getText().equals("") && String.valueOf(Login.txtPassword.getPassword()).equals(""))*/) {
+				Login.window.dispose();
+			} else {
+				JOptionPane.showMessageDialog(Login.window,
+						"Usuario o contraseña incorrectos",
+						"Gestión de Login", JOptionPane.PLAIN_MESSAGE);
+			}
+		} catch (IOException e) {
+			JOptionPane.showMessageDialog(Login.window,
+					"No se ha podido verificar las credenciales, si sigue ocurriendo contacte con el equipo de desarrollo",
+					"Gestión de Login", JOptionPane.PLAIN_MESSAGE);
+			e.printStackTrace();
 		}
+		
 	}
-
-	public static void insertUser() {
-		Login.window.dispose();
-	}
-
 }
